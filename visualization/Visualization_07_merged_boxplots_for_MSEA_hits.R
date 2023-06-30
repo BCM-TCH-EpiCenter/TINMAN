3844365665f68c1427839ddaf6bcf24836866b74
schrawj <schrawj@gmail.com>
2023-06-30 15:30:08 -0500

    Add p-values
    Add p-values

--- visualization/Visualization_07_merged_boxplots_for_MSEA_hits.R
+++ visualization/Visualization_07_merged_boxplots_for_MSEA_hits.R
@@ -10,6 +10,8 @@ metabs <- readRDS('//smb-main.ad.bcm.edu/genepi3/JeremySchraw/TINMAN/Datasets/TI
 metab.metadata <- readRDS('//smb-main.ad.bcm.edu/genepi3/JeremySchraw/TINMAN/Datasets/TINMAN_merged_feces_metadata.rds') %>% 
   mutate(CHEMICAL_NAME = str_trim(tolower(CHEMICAL_NAME), 'both'))
 
+#' P-values for 
+
 #' Clinical metadata like group and timepoint.
 clinical.metadata <- readRDS('//smb-main.ad.bcm.edu/genepi3/JeremySchraw/TINMAN/Datasets/TINMAN_merged_clinical_data.rds')
 
@@ -33,6 +35,39 @@ for (i in 1:nrow(msea.hits)){
     filter(CHEMICAL_NAME %in% index.metabs) %>% 
     pull(CHEM_ID)
   
+  #' Run t-tests on each metabolite so that we can annotate with the p-value.
+  test.data <- metabs %>% 
+    select(PARENT_SAMPLE_NAME, all_of(ids)) %>% 
+    left_join(select(clinical.metadata, PARENT_SAMPLE_NAME, GROUP_NAME, TIMEPOINT)) %>% 
+    filter(TIMEPOINT == my.timepoints[i])
+  
+  test.results <- tibble()
+  
+  for (j in seq_along(ids)){
+    
+    index.test.data <- select(test.data, GROUP_NAME, ids[j])
+    
+    #' Only metabolite abundance columns. Used to find y-axis value at which to annotate plots that are produced below.
+    metabs.only <- test.data %>% 
+      select(starts_with('C'))
+    
+    names(index.test.data) <- c('group','metab')
+    
+    test <- t.test(metab ~ group, data = index.test.data)
+    
+    test.result <- tibble(id = ids[j],
+                          timepoint = my.timepoints[i],
+                          t.test.p.value = paste0('p=', round(test$p.value, 2)),
+                          value = max(metabs.only)*0.8) # 'value' will become the point on the y-axis at which the p-value will be plotted.
+    
+    test.results <- rbind(test.results, test.result)
+    
+  }
+  
+  test.results <- test.results %>% 
+    mutate(name = index.metabs,
+           GROUP_NAME = 1.5) # point along the x-axis at which the p-value will be plotted. This puts it between the two groups.
+  
   plot.data <- metabs %>% 
     select(PARENT_SAMPLE_NAME, all_of(ids)) 
   
@@ -45,9 +80,11 @@ for (i in 1:nrow(msea.hits)){
   index.plot.data <- plot.data %>% 
     filter(TIMEPOINT == my.timepoints[i])
   
-  plot <- ggplot(data = index.plot.data, aes(x = GROUP_NAME, y = value, fill = GROUP_NAME)) + 
+  plot <- ggplot(data = index.plot.data, aes(x = GROUP_NAME, y = value)) + 
     
-    geom_boxplot() +
+    geom_boxplot(aes(fill = GROUP_NAME)) +
+    
+    geom_text(aes(label = t.test.p.value), data = test.results, size = 5, fontface = 'bold') +
     
     theme_classic() + 
     
@@ -82,3 +119,5 @@ for (i in 1:nrow(msea.hits)){
   dev.off()
   
 }
+
+# Scratch paper -----------------------------------------------------------
